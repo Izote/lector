95c82a0434ce6f2daa9ec297d154176c883f7939
Izote <munguiamv@gmail.com>
2022-12-04 08:34:43 -0500

    rerouted write output to `documents` directory

--- data/write.py
+++ data/write.py
@@ -1,9 +1,11 @@
+from os.path import join
 from time import time
-from utils import *
+from utils import DOC_DIR
 
 
 def write(
         content: list,
+        directory: str = DOC_DIR,
         filename: str = None,
         extension: str = 'txt'
 ) -> None:
@@ -12,6 +14,7 @@ def write(
     Unless provided, the filename is stored as a timestamp.
 
     :param content: a List or List-like object instance.
+    :param directory: storage directory for file output.
     :param filename: the output filename (minus extension), if provided.
     :param extension: desired file extension, defaults to 'txt'.
     :return: none
@@ -19,7 +22,7 @@ def write(
     if filename is None:
         filename = int(time())
 
-    filepath = join(LIB_DIR, f'{filename}.{extension}')
+    filepath = join(DOC_DIR, f'{filename}.{extension}')
     with open(filepath, 'w') as file:
         for line in content:
             print(line, file=file)
