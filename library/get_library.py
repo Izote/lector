aeea83664615c7ef7d611a06c54ad280493895ee
Izote <munguiamv@gmail.com>
2022-12-04 08:28:38 -0500

    first

---
+++ library/get_library.py
@@ -0,0 +1,15 @@
+from __future__ import annotations
+from os.path import join
+from pickle import load
+from typing import TYPE_CHECKING
+from utils import LIB_DIR
+
+if TYPE_CHECKING:
+    from library.library import Library
+
+
+def get_library(filename: str) -> Library:
+    with open(join(LIB_DIR, filename), 'rb') as file:
+        lib = load(file)
+
+    return lib
