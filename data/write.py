ca2d2c8bc7036f630e8c3702993dae1c342701a9
Izote <munguiamv@gmail.com>
2022-12-04 14:03:55 -0500

    added encoding argument

--- data/write.py
+++ data/write.py
@@ -7,7 +7,8 @@ def write(
         content: list,
         directory: str = DOC_DIR,
         filename: str = None,
-        extension: str = 'txt'
+        extension: str = 'txt',
+        encoding: str = 'utf-8'
 ) -> None:
     """
     Takes a List and writes it as a file ('txt' by default).
@@ -17,12 +18,13 @@ def write(
     :param directory: storage directory for file output.
     :param filename: the output filename (minus extension), if provided.
     :param extension: desired file extension, defaults to 'txt'.
+    :param encoding: desired encoding type, defaults to 'utf-8'.
     :return: none
     """
     if filename is None:
         filename = int(time())
 
     filepath = join(DOC_DIR, f'{filename}.{extension}')
-    with open(filepath, 'w') as file:
+    with open(filepath, 'w', encoding=encoding) as file:
         for line in content:
             print(line, file=file)
