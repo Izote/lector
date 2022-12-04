f1a891caf6f4ee598f5a4bbd787fa1fe8fe2fc96
Izote <munguiamv@gmail.com>
2022-12-04 08:29:17 -0500

    removed default pattern to encourage generalized use

--- data/clean_text.py
+++ data/clean_text.py
@@ -1,9 +1,9 @@
-from re import sub
+from re import sub, UNICODE
 
 
 def clean_text(
         raw_text: str | list,
-        pattern: str = r'\\\\.|[^a-zA-Z0-9\-\.\(\), ]'
+        pattern: str
 ) -> str | list:
     """
     Accepts raw text as a String or List (of Strings), returning output of
@@ -12,11 +12,11 @@ def clean_text(
     are removed from List outputs.
 
     :param raw_text: raw text as a String or List (of Strings).
-    :param pattern: a default pattern
+    :param pattern: a regular expression pattern to be removed from `raw_text`.
     :return: cleaned text as a String or List (of Strings).
     """
     def remove_chars(text: str) -> str:
-        return sub(pattern, '', text).strip()
+        return sub(pattern, '', text, UNICODE).strip()
 
     if isinstance(raw_text, list):
         return [t for t in [remove_chars(rt) for rt in raw_text] if t != '']
